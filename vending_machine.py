296643a77fe4fbeb5a837998bbf81edad4f02981
Delphi#7393 <jeremystjean@gmail.com>
2020-11-17 12:06:01 -0500

    Updated Vending Machine command and data

--- vending_machine.py
+++ vending_machine.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import os
 import io
 import random
+import asyncio
 
 
 
@@ -15,12 +16,14 @@ async def vending_machine(ctx):
     
     with open(r"C:\Users\jerem\OneDrive\Bureau\SquidBot\titles.txt","r", encoding="utf8") as tf:
         the_title = random.choice(tf.readlines())
-
+    message = await ctx.send("you insert a coin in the machine...")
+    await asyncio.sleep(3) # DRAMATIC TENSION
     embed=discord.Embed(title="**" + the_title + "**", color=0x47e18a)
     embed.set_thumbnail(url="https://media.discordapp.net/attachments/280298381807714304/776122672898768956/vending_machine.png")
     embed.add_field(name="_ _", value= the_prize, inline=False)
     embed.set_footer(text="VoidCorp™")
-    await ctx.send(embed=embed)
+    await message.edit(content="", embed=embed)
+
 
 def setup(bot):
     bot.add_command(vending_machine)
